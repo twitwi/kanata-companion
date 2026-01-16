0a6eb6cfff5682dae26265eb16411e6bb66bf2ea
Rémi Emonet <remi.emonet@reverse--com.heeere>
2026-01-16 23:47:46 +0100

    SVG keyboard, with raw loading

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import keyboard from './kb/iso-azerty.svg?raw'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,11 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div v-html="keyboard"></div>
 </template>
 
-<style scoped></style>
+<style>
+#key-slots :not(.added) {
+  stroke: transparent;
+}
+</style>
