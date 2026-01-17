f278d352d21e37640f4c99cf320f879b4619bd40
Rémi Emonet <remi.emonet@reverse--com.heeere>
2026-01-17 03:07:20 +0100

    Connect to kanata + manage to round trip events

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,66 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::BufRead;
+use tauri::{AppHandle, Emitter};
+
+// a set of constants / config
+const HOST: &str = "localhost";
+const PORT: u16 = 42041;
+
+  // create a new thread that connects
+  // to the tcp server at HOST:PORT, reconnecting if the connection is lost
+  // it reads json messages (linewise)
+
+#[tauri::command]
+fn ping_pong(app: AppHandle) {
+    app.emit("ping-pong", "init1").unwrap();
+}
+#[tauri::command]
+fn start_kanata_listener(app: AppHandle) {
+    app.emit("kanata-message", "init1").unwrap();
+    
+    std::thread::spawn(move || {
+    app.emit("kanata-message", "init2").unwrap();
+        loop {
+            match std::net::TcpStream::connect((HOST, PORT)) {
+                Ok(stream) => {
+                    let reader = std::io::BufReader::new(stream.try_clone().unwrap());
+                    for line in reader.lines() {
+                    //loop {
+                    //  let line = reader.read_line();
+                        match line {
+                            Ok(json_msg) => {
+                                // process the json message
+                                app.emit("kanata-message", "before").unwrap();
+                                println!("Received: {}", json_msg);
+                                app.emit("kanata-message", json_msg).unwrap();
+                            }
+                            Err(e) => {
+                                eprintln!("Error reading from stream: {}", e);
+                                break;
+                            }
+                        }
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to connect: {}", e);
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                }
+            }
+        }
+    });
+}
+
+
+
+
 fn main() {
-  app_lib::run();
+
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![start_kanata_listener, ping_pong])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+  //start_kanata_listenera();
+  //app_lib::run();
 }
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
 import KB from './kb/KB.vue'
+import { invoke } from '@tauri-apps/api/core';
+import { listen } from '@tauri-apps/api/event';
+//import { emit } from '@tauri-apps/api/event'
+
+import { ref } from 'vue';
+
+const layer = ref('default');
+
+listen('kanata-message', (event) => {
+  console.log("Received kanata message:", event);
+  const msg = JSON.parse(event.payload ?? '{}');
+  const l = msg?.LayerChange?.new
+  console.log("Layer change:", l)
+  if (l) {
+    layer.value = l
+  }
+});
+
+listen('ping-pong', (event) => {
+  console.log("Received ping-pong message:", event);
+});
+
+invoke('start_kanata_listener')
+
 </script>
 
 <template>
-  <h1>Don't loose your mind!!!!!</h1>
   <KB :layer="'singlehandmode'"></KB>
+  <div @click="invoke('ping_pong')">{{ layer }}</div>
 </template>
 
 <style>
